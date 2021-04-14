89ec80240c1031ee09881af95cc2246ed4d790c8
Vili Raunola <raunolavili@gmail.com>
2021-04-14 01:41:13 -0700

    error on perror

---
+++ my-cat.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define  _POSIX_C_SOURCE 200809L
+
+void read_File(char *file_name);
+
+int main(int argc, char *argv[]){
+
+    for(int i = 0; i < argc;i++){
+        read_File(argv[i]);
+    }
+
+    return(0);
+}
+
+
+void read_File(char *file_name){
+    char *line = NULL;
+    size_t buffer_size = 0;
+    FILE *file = NULL;
+
+    if ((file = fopen(file_name, "r")) == NULL) {
+        perror("my-cat: cannot open file");
+        fprintf(stdout,"\n");
+        exit(1);
+    }
+
+    while(getline(&line, &buffer_size, file) != -1){ 
+        fprintf(stdout, "%s", line);
+    }
+
+    free(line);
+    fclose(file);
+}
