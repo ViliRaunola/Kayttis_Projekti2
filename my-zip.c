c4cd13495190dfd4098237564b7ec1eb53a01922
Vili Raunola <raunolavili@gmail.com>
2021-04-16 01:33:45 -0700

    my-zip is commented

--- my-zip.c
+++ my-zip.c
@@ -1,3 +1,10 @@
+/* my-zip.c */
+/* Jesse Pasanen 0545937 */
+/* Vili Raunola 0543366 */
+
+/* Source: https://stackoverflow.com/questions/59014090/warning-implicit-declaration-of-function-getline */
+/* error with getline() solved with '#define  _POSIX_C_SOURCE 200809L' */
+#define  _POSIX_C_SOURCE 200809L
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -7,20 +14,25 @@ void compress_File(FILE *file);
 
 int main(int argc, char *argv[]){
 
+    //If no arguments are given, a message is shown to the user.
     if(argc == 1){
         fprintf(stdout, "wzip: file1 [file2 ...]\n");   //The error message differs between assigment and tests
         exit(1);
     }
 
+    //If there is only one input file that needs to be compressed it will be sent to compress_File()-function.
+    //If there are multiple files to be compressed they are read one by one to a temporary file after which the 
+    //temporary file will get compressed.
     if(argc < 3){
         FILE *fp;
         if ((fp = fopen(argv[1], "r")) == NULL) {
             fprintf(stdout,"wzip: cannot open file\n"); //The error message differs between assigment and tests
             exit(1);
         }
+        //Reads and zips the given file
         compress_File(fp);
     }else{
-        //How to use temporary file in c: https://www.geeksforgeeks.org/tmpfile-function-c/
+        //How to initialize temporary file in c: https://www.geeksforgeeks.org/tmpfile-function-c/
         FILE *fp_tempFile = tmpfile(); 
 
         //Reading all the given files to a temporary file
@@ -28,7 +40,7 @@ int main(int argc, char *argv[]){
             create_Tempfile(argv[i], fp_tempFile);
         }
 
-        //How to use temporary file in c: https://www.geeksforgeeks.org/tmpfile-function-c/
+        //Placing the pointer back to the beginning of the temporaryfile: https://www.geeksforgeeks.org/tmpfile-function-c/
         rewind(fp_tempFile);
 
         //Reads and zips the temporary file
@@ -38,7 +50,7 @@ int main(int argc, char *argv[]){
     return(0);
 }
 
-
+//This function copies the contents of a given file to a temporary file using getline.
 void create_Tempfile(char *file_name, FILE *fp_tempFile){
     char *line = NULL;
     ssize_t line_size;
@@ -50,23 +62,27 @@ void create_Tempfile(char *file_name, FILE *fp_tempFile){
         exit(1);
     }
 
+    //getline() automatically updates the size of 'line' and 'n' to
+    //fit the line that is beign read. They must be set to NULL and 0 before calling.
+    //returns -1 when exiting.
+    //Source: man getline
     while((line_size = getline(&line, &buffer_size, file)) != -1){ 
         fprintf(fp_tempFile, "%s", line);
-        //create_Zip(line, line_size);
     }
 
     free(line);
     fclose(file);
 }
 
-
+//This function does the compression by comparing the characters one by one.
 void compress_File(FILE *file){
-    char c; 
+    char c; //Current character
     char pre_char;
-    int counter = 0;
+    int counter = 0;    //Counter that keeps track of the repeating characters
 
+    //Checks if the is empty or not.
     if(feof(file)){
-        fprintf(stdout,"wzip: the file is empty\n"); //The error message differs between assigment and tests
+        fprintf(stderr,"wzip: the file is empty\n"); 
         exit(1);
     }
 
@@ -75,22 +91,22 @@ void compress_File(FILE *file){
     pre_char = c;
 
     while(1){ 
+        //If the end of the file is reached the remaining variables aka counter and current character are printed to standard output.
         if(feof(file)){
             fwrite(&counter, 4, 1,stdout);
             fprintf(stdout,"%c", pre_char);
             break;
         }
+        //If the previous and current character match, a counter that keeps tracks of the repeating characters gets increased by one.
         if(pre_char == c){
             counter++;
-        }else{
+        }else{ //If the current and previous character don't match, the counter and previous character will be printed aka the compressed information.
             fwrite(&counter, 4, 1,stdout);
-            fflush(stdout);
             fprintf(stdout,"%c", pre_char);
-            fflush(stdout);
             pre_char = c;
             counter = 1;
         }
-
+        //Gets the next character from the file.
         c = fgetc(file);
     }
 
